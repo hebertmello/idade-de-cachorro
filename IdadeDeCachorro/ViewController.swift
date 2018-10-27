8659a7b56caec99862dcb033922c79abf47bb0fa
Hebert <hebert.mello@gmail.com>
2018-10-27 19:03:00 -0300

    Repositorio

---
+++ IdadeDeCachorro/ViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ViewController.swift
+//  IdadeDeCachorro
+//
+//  Created by Hebert Falcão on 22/04/2018.
+//  Copyright © 2018 hebertfalcao. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var legendaResultado: UILabel!
+    
+    @IBOutlet weak var campoIdadeCachorro: UITextField!
+    
+    @IBAction func descobrirIdade(_ sender: Any) {
+        let idade = Int(campoIdadeCachorro.text!)! * 7
+        legendaResultado.text = "A idade do cachorro é: " + String(idade)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
